629d27b20fb60eca996593825d021e213d5eac05
TuNombre <yo@yo.com>
2026-07-17 18:59:46 -0600

    Añadido diseño visual activity_main

---
+++ app/src/main/java/com/auraturbo/azul/MainActivity.java
@@ -0,0 +1,13 @@
+package com.auraturbo.azul;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Aquí conectaremos el diseño visual más adelante
+    }
+}
+
